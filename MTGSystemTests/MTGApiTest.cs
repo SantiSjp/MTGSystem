c37fee1a287a3e9973a7f8493657619e290a5c79
santisjp <santisjp@live.com>
2020-09-17 10:43:00 -0300

    Teste unitário API #1

--- MTGSystemTests/MTGApiTest.cs
+++ MTGSystemTests/MTGApiTest.cs
@@ -9,18 +9,18 @@ namespace MTGSystemTests
         [Fact]
         public void TestCard()
         {
-            /*
+            
              
             // Arrange
             var mtgapi = new MTGSetRepository();
 
             // Act
-            var list = mtgapi.BuscarSet();
+            mtgapi.BuscarSet();
 
             //Assert
-            Assert.NotEmpty(list);
-            
-             */
+            //Assert.NotEmpty(list);
+          
+             
 
             Assert.Equal(1, 1);
 
